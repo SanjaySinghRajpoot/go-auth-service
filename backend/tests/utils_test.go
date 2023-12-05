b4b3fa2f43e35dd8951f9f7e13ce350c1e856429
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-12-05 19:17:37 +0530

    Test: adding test for HashPassword

---
+++ backend/tests/utils_test.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/SanjaySinghRajpoot/backend/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HashPassword(t *testing.T) {
+
+	hashedPassword, err := utils.HashPassword("test")
+	assert.Nil(t, err)
+	assert.NotNil(t, hashedPassword)
+
+	// If passwords are not same then error will be returned
+	error := utils.VerifyPassword(hashedPassword, "test")
+
+	assert.Nil(t, error)
+}
